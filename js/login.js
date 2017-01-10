8357bb4e2fd79d3ba43d565980aa71a7676dad1e
Victoriia Borysovska <vmiracle13@gmail.com>
2017-01-10 18:35:06 +0200

    edit em and % parameters - js

--- js/login.js
+++ js/login.js
@@ -10,15 +10,6 @@
     var logImgSymbol = document.getElementsByClassName('loginImg')[0];
     var form = document.getElementsByClassName('login-form')[0];
     var spanLoginSymbol = document.querySelectorAll('span')[3];
-/*
-    loginSymbol.onclick = logImgSymbol.onclick = function(event) {
-      var target = event.target;
-      target.classList.toggle('greenBckgr');
-      spanLoginSymbol.classList.toggle('loginImg');
-      spanLoginSymbol.classList.toggle('changeSymbol');
-      form.classList.toggle('hide');
-    };*/
-
 
     logImgSymbol.addEventListener("click", handler1);
     loginSymbol.addEventListener( "click", handler1);
@@ -47,3 +38,39 @@
     }
   };
 }());
+
+/*
+ ;(function() {
+
+ window.onload = function() {
+
+ var loginSymbol = document.getElementsByClassName('login')[0];
+ var logImgSymbol = document.getElementsByClassName('loginImg')[0];
+ var form = document.getElementsByClassName('login-form')[0];
+ var spanLoginSymbol = document.querySelectorAll('span')[3];
+
+ loginSymbol.onclick = logImgSymbol.onclick = function(event) {
+ var target = event.target;
+ target.classList.toggle('greenBckgr');
+ spanLoginSymbol.classList.toggle('loginImg');
+ spanLoginSymbol.classList.toggle('changeSymbol');
+ form.classList.toggle('hide');
+ };
+
+ var sidebar = document.getElementsByClassName('sidebar')[0];
+ var buttonSidebar = sidebar.lastElementChild.lastElementChild;
+ var imgSidebar = sidebar.firstElementChild;
+ var textOnImg = sidebar.childNodes[3];
+
+ buttonSidebar.onclick = function() {
+ buttonSidebar.innerHTML = 'I liked';
+ buttonSidebar.classList.toggle('onclickButtonA');
+ buttonSidebar.classList.toggle('onclickButtonBeforeA');
+ imgSidebar.classList.toggle('onclickButtonImg');
+ textOnImg.classList.toggle('hide');
+ }
+ };
+
+ }());
+
+*/
